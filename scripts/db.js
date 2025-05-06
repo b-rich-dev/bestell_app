eb8c9af0ef334372de8d528803604fd1036f9abf
b-rich-dev <eugen.birich@gmail.com>
2025-05-06 09:45:55 +0200

    db

--- scripts/db.js
+++ scripts/db.js
@@ -1,4 +1,81 @@
-let myDishes = []  
+let myDishes = [
+    {
+        "Vorspeisen": [
+            {
+                "name": "Bruschetta",
+                "price": 6.90,
+                "description": "Knusprige Brotscheiben mit Tomaten, Mozzarella und Basilikum",
+                "image": "bruschetta.jpg",
+                "amount": 0
+            },
+            {
+                "name": "Garnelen-Pfännchen",
+                "price": 16.50,
+                "description": "Gebratene Garnelen in pikantem Knoblauch-Oliven-Öl",
+                "image": "shrimps_in_the_pan.jpg",
+                "amount": 0
+            },
+            {
+                "name": "Carpaccio",
+                "price": 15.50,
+                "description": "Hauchdünne rohe Rinderfiletscheiben, frischer Rucola und Grana Padano",
+                "image": "carpaccio_1.jpg",
+                "amount": 0
+            }
+        ],
+        "Hauptgerichte": [
+            {
+                "name": "Pizza Margherita",
+                "price": 10.90,
+                "description": "Tomatensauce, Mozzarella und frischer Basilikum",
+                "image": "pizza_margherita.jpg",
+                "amount": 0
+            },
+            {
+                "name": "Pizza Salami",
+                "price": 12.50,
+                "description": "Tomatensauce, Mozzarella und italienische Salami",
+                "image": "pizza_salami_2.jpg",
+                "amount": 0
+            },
+            {
+                "name": "Pizza Funghi",
+                "price": 10.90,
+                "description": "Tomatensauce, Mozzarella und frische Champignons",
+                "image": "pizza_funghi.jpg",
+                "amount": 0
+            }
+        ],
+        "Desserts": [
+            {
+                "name": "Tiramisu",
+                "price": 7.90,
+                "description": "Klassisches italienisches Dessert mit Mascarpone und Espresso",
+                "image": "tiramisu.jpg",
+                "amount": 0
+            },
+            {
+                "name": "Cannoli",
+                "price": 4.90,
+                "description": "Knusprige, gefüllte Teigröllchen mit süßer Ricotta-Creme.",
+                "image": "cannoli.jpg",
+                "amount": 0
+            },
+            {
+                "name": "Limoncello Gelato",
+                "price": 6.90,
+                "description": "Zitroniges Eis mit dem Aroma des italienischen Limoncello-Likörs.",
+                "image": "limoncello_gelato.jpg",
+                "amount": 0
+            }
+        ]
+    }
+]
 
 
 let mybasket = []
+
+let mytrash = []
+
+let mySubtotal = 0.00
+let mytotal = 0.00
